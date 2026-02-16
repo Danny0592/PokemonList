c6c3458634bcc1db1ac38d3f638b45a840c22b06
Danny0592 <155591032+Danny0592@users.noreply.github.com>
2026-02-16 15:33:23 -0600

    Initial Commit

---
+++ kemonList/kemonListApp.swift
@@ -0,0 +1,17 @@
+//
+//  kemonListApp.swift
+//  kemonList
+//
+//  Created by daniel ortiz millan on 16/02/26.
+//
+
+import SwiftUI
+
+@main
+struct kemonListApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ kemonListTests/kemonListTests.swift
@@ -0,0 +1,17 @@
+//
+//  kemonListTests.swift
+//  kemonListTests
+//
+//  Created by daniel ortiz millan on 16/02/26.
+//
+
+import Testing
+@testable import kemonList
+
+struct kemonListTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
